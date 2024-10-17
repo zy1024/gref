89b355e526caccdef8318d2a341feceb0c41e068
layman <layman@91aoyou.com>
2024-10-17 15:19:42 +0800

    feat: extend IsBasicType to accept multiple kinds

--- utils/isBasicType.go
+++ utils/isBasicType.go
@@ -2,17 +2,20 @@ package utils
 
 import "reflect"
 
-// IsBasicType checks if the given kind is a basic type.
-func IsBasicType(kind reflect.Kind) bool {
-	switch kind {
-	case reflect.Bool,
-		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
-		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
-		reflect.Float32, reflect.Float64,
-		reflect.Complex64, reflect.Complex128,
-		reflect.String:
-		return true
-	default:
-		return false
+// IsBasicType checks if all given kinds are basic types.
+func IsBasicType(kinds ...reflect.Kind) bool {
+	for _, kind := range kinds {
+		switch kind {
+		case reflect.Bool,
+			reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+			reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
+			reflect.Float32, reflect.Float64,
+			reflect.Complex64, reflect.Complex128,
+			reflect.String:
+			continue
+		default:
+			return false
+		}
 	}
+	return true
 }
