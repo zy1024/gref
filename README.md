a7bb94e35f567375144b8bc9e20d3155738e4f0d
layman <layman@91aoyou.com>
2024-10-18 11:18:59 +0800

    feat(docs): The README file has been updated with a description of the supported types and efficiency of the replication function, and the sample code has been updated.

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 # gref
 gref is a utility library in Golang that utilizes reflection for data assignment. The main function is to assign values between two parameters, such as two different structs.
 
-### Installation
+## Install
 ```
 go get github.com/zy1024/gref
 ```
 
-### Document Language : **[English](README.md), [中文](README_CN.md).**
+## Document Language : **[English](README.md), [中文](README_CN.md).**
 
-## Features
+# Features
 
-### 1. Copy
+## 1. Copy
 The `Copy(src,dst)` function requires two pointers, such as pointers to structs or slices. It will use reflection to assign the value of src to dst. The assignment of structs is based on matching field names, and only non-empty fields in dst will be assigned.
+
+### Support type
+Support for basic data types, structs, Pointers, slices.
+
+### Operating efficiency
+More efficient than traditional json.Marshal and json.Unmarshal methods. With the same data, the efficiency is about 3-4 times.
+
 ### Example
 The sample code can be seen in the `TestCopy` method in `example_test.go`.
 
@@ -20,4 +27,4 @@ The sample code can be seen in the `TestCopy` method in `example_test.go`.
 Official release, supporting assignment operations for structs, pointers, slices, and basic data types.
 
 ### Next Development Steps
-1. Add support for other types, such as channels.
+1. Add support for other types, such as channel and map.
--- example_test.go
+++ example_test.go
@@ -4,6 +4,7 @@ package gref
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 type PersonContainAddress struct {
@@ -18,62 +19,84 @@ type Person struct {
 }
 
 type Src struct {
-	Name     string
-	Age      string
-	Password string
-	Friends  []PersonContainAddress
-	Parent   PersonContainAddress
-	Brother  *PersonContainAddress
-	Sister   []*PersonContainAddress
-	Teacher  []Person
+	Name     string                  // same type and name as dst, will be copied directly
+	Age      string                  // same name but different type, will be converted to dst type (int)
+	Password string                  // dst has no Password field, will be ignored
+	Friends  []PersonContainAddress  // both are slices of structs, but structs have different fields; will copy and convert the same fields
+	Parent   PersonContainAddress    // both are structs, but have different fields; will copy and convert the same fields
+	Brother  *PersonContainAddress   // both are pointers to structs, but structs have different fields; will copy and convert the same fields
+	Sister   []*PersonContainAddress // both are slices of pointers to structs, but structs have different fields; will copy and convert the same fields
+	Teacher  []Person                // both are slices, but have different data types; this will not be assigned and will be ignored
 }
 
 type Dst struct {
 	Name    string
-	Age     float64
+	Age     int
 	Friends []Person
 	Parent  Person
 	Brother *Person
 	Sister  []*Person
-	Teacher []int
+	Teacher []string
 }
 
 // example
 func TestCopy(t *testing.T) {
 	src := &Src{
-		Name:     "A",                             // same type and name as dst , will be copied
-		Age:      "12",                            // same name but different type, will be converted to dst type
-		Password: "123456",                        // dst has no Password field, will be ignored
-		Friends:  make([]PersonContainAddress, 0), // dst has same Friends field but different type, and fewer fields, the same fields will be copied and converted
-		Parent: PersonContainAddress{ // same name but different type, will be converted to dst type
+		Name:     "A",
+		Age:      "12",
+		Password: "123456",
+		Parent: PersonContainAddress{
 			Name: "parent-A",
 			Age:  32,
-			Addr: "address-A",
+			Addr: "address-A1",
 		},
-		Brother: &PersonContainAddress{ // dst has data, will be ignored
+		Brother: &PersonContainAddress{
 			Name: "brother-A",
 			Age:  13,
-			Addr: "address-A",
+			Addr: "address-A1",
 		},
 	}
-	src.Friends = append(src.Friends, PersonContainAddress{Name: "B", Age: 13, Addr: "address-B"})
-	src.Friends = append(src.Friends, PersonContainAddress{Name: "C", Age: 14, Addr: "address-C"})
-	src.Sister = append(src.Sister, &PersonContainAddress{Name: "sister-A1", Age: 15, Addr: "address-E"})
-	src.Sister = append(src.Sister, &PersonContainAddress{Name: "sister-A2", Age: 16, Addr: "address-D"})
+	src.Friends = append(src.Friends, PersonContainAddress{Name: "friend-A1", Age: 13, Addr: "address-A2"})
+	src.Friends = append(src.Friends, PersonContainAddress{Name: "friend-A2", Age: 14, Addr: "address-A3"})
+	src.Sister = append(src.Sister, &PersonContainAddress{Name: "sister-A1", Age: 15, Addr: "address-A1"})
+	src.Sister = append(src.Sister, &PersonContainAddress{Name: "sister-A2", Age: 16, Addr: "address-A1"})
 	src.Teacher = append(src.Teacher, Person{Name: "teacher-A", Age: 30})
 
 	dst := &Dst{
-		Brother: &Person{
-			Name: "",
+		Brother: &Person{ // dst has data, src data will be ignored
+			Name: "Ignore",
 			Age:  10,
 		},
 	}
+
+	startTime := time.Now()
+
+	// Operating efficiency test
+	//for i := 0; i < 1; i++ {
+	//	srcBytes, err := json.Marshal(src)
+	//	if err != nil {
+	//		t.Error(err)
+	//	}
+	//	err = json.Unmarshal(srcBytes, dst)
+	//	if err != nil {
+	//		t.Error(err)
+	//	}
+	//	err := Copy(src, dst)
+	//	if err != nil {
+	//		t.Error(err)
+	//		return
+	//	}
+	//}
 
 	err := Copy(src, dst)
 	if err != nil {
 		t.Error(err)
 		return
 	}
+
+	endTime := time.Now()
+
+	fmt.Println("time:", endTime.Sub(startTime))
 
 	fmt.Println(src)
 	fmt.Println("---------------------------------------")
